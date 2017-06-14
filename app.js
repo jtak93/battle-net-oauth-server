03c523b6a97212084ed5c5bc4f6daa37177449f0
james tak <jtak93@gmail.com>
2017-06-14 16:42:08 -0700

    add bnet passport and cors

--- app.js
+++ app.js
@@ -12,13 +12,41 @@ var auth = require('./routes/auth-route');
 var config = require('./config');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
+var cors = require('cors');
 
 var app = express();
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+app.use(cors({origin: 'http://localhost:3000', credentials: true}));
+
+passport.serializeUser(function(user, done) {
+    done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+    done(null, obj);
+});
+
+var BnetStrategy = require('passport-bnet').Strategy;
+
+// TODO: MOVE to ENV variables
+var BNET_ID = 'twqb6zh97nzgvxa9ffrs2mx8xqvjuw52';
+var BNET_SECRET = 'EjBrrbAJkeuKJzG8BpGSPwUR8BBune2Z';
+
+// Use the BnetStrategy within Passport.
+passport.use(new BnetStrategy({
+    clientID: BNET_ID,
+    clientSecret: BNET_SECRET,
+    callbackURL: "https://battle-net-oauth-server.herokuapp.com/auth/bnet/callback"
+}, function(accessToken, refreshToken, profile, done) {
+    console.log('bnet passport')
+    process.nextTick(function () {
+        return done(null, profile);
+    });
+}));
 
 // Connect to MongoDB
 mongoose.connect(config.mongo.url)
@@ -36,7 +64,7 @@ console.log(`Connected to: ${config.mongo.url}`)
 // }));
 app.use(session({
   store: new MongoStore({mongooseConnection: mongoose.connection}),
-  secret: '342080a8c387b069a0d5dfac5ac89b58',
+  secret: 'blizzard',
   resave: false,
   saveUninitialized: true
 }))
@@ -44,7 +72,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/api/auth', auth);
+app.use('/auth', auth);
 app.use('/api/v1', api);
 
 // catch 404 and forward to error handler
--- routes/auth-route.js
+++ routes/auth-route.js
@@ -1,8 +1,25 @@
 var express = require('express');
 var passport = require('passport');
 var router = express.Router();
-var auth = require('../controllers/auth-controller');
 
-router.get('/basic', passport.authenticate('basic', {session: true}), auth.success);
+router.get('/bnet',
+        function(req, res, next) {
+          console.log('/auth/bnet route hit'),
+          next()
+        },
+        passport.authenticate('bnet'));
+
+router.get('/bnet/callback',
+        passport.authenticate('bnet', { failureRedirect: '/' }),
+        function(req, res){
+          console.log('bnet callback')
+        });
+
+router.get('/fail', function(req, res, next) {
+  res.send({
+    status: 401,
+    message: 'Failed to Login'
+  })
+})
 
 module.exports = router;
